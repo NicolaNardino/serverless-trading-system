6a3958485d21e40640ab4060ee9eb6344bdea38d
nicolanardino <nicola.nardino@nicolanardino.net>
2022-02-04 19:49:42 +0100

    Added DynamoDB/ UpdateCommand.

--- lambda-layer/nodejs/src/dependencies.js
+++ lambda-layer/nodejs/src/dependencies.js
@@ -2,7 +2,7 @@ import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { SSMClient, GetParameterCommand, GetParametersCommand } from "@aws-sdk/client-ssm";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb"; 
-import { DynamoDBDocumentClient, PutCommand, GetCommand, QueryCommand  } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, PutCommand, GetCommand, QueryCommand, UpdateCommand  } from "@aws-sdk/lib-dynamodb";
 import { randomUUID } from 'crypto';
 
 export {
@@ -18,5 +18,6 @@ export {
     PutCommand,
     GetCommand,
     QueryCommand,
+    UpdateCommand,
     randomUUID
 }
