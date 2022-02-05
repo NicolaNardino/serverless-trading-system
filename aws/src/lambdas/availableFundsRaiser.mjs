56f368615b540dbd1e8768550638d1caf0e3c1c3
nicolanardino <nicola.nardino@nicolanardino.net>
2022-02-05 19:45:08 +0100

    Added Lambda function that subscribes to messages containing failed orders due to customers being low on funds, and raises those funds to an arbitrary value of 1M.

---
+++ aws/src/lambdas/availableFundsRaiser.mjs
@@ -0,0 +1,35 @@
+import { DynamoDBClient, DynamoDBDocumentClient, UpdateCommand } from '/opt/nodejs/src/dependencies.js';
+
+const region = { region: 'us-east-2' };
+const dynamoDBClient = new DynamoDBClient(region);
+const ddbDocClient = DynamoDBDocumentClient.from(dynamoDBClient);
+
+const newFunds = 1000000;
+
+export async function handler(event) {
+    console.log(JSON.stringify(event));
+    for (const record of event.Records) {
+        const customerIds = [...new Set(JSON.parse(record.Sns.Message).map(order => order.customerId))]; //customerIds that need funds raised.
+        for (const customerId of customerIds) {
+            const params = {
+                    TableName: "trades",
+                    Key: {
+                        PK: "CUST#"+customerId,
+                        SK: "CUST#"+customerId,
+                    },
+                    ExpressionAttributeValues: {
+                        ':NewFunds': newFunds,
+                        ':Now': Date.now()
+                    },
+                    UpdateExpression: 'SET RemainingFunds = :NewFunds, Updated = :Now'
+                };
+            await ddbDocClient.send(new UpdateCommand(params));
+            console.log("Updated remaining funds of customerId", customerId, "to", newFunds);
+        }
+    }
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify('Available funds raised'),
+    };
+}
