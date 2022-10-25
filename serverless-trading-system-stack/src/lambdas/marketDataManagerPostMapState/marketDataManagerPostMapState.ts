58b2efdab5169f0210588c82348a24baf3088b2f
nicolanardino <nicola.nardino@nicolanardino.net>
2022-10-25 18:32:00 +0200

    Added choices to market data manager state machine.

--- serverless-trading-system-stack/src/lambdas/marketDataManagerPostMapState/marketDataManagerPostMapState.ts
+++ serverless-trading-system-stack/src/lambdas/marketDataManagerPostMapState/marketDataManagerPostMapState.ts
@@ -1,3 +1,12 @@
-export async function handler(input: { tickers: {ticker: string; itemsLoaded: number}[] }): Promise<void> {
-  console.log(input);
+export async function handler(input: Input): Promise<Output> {
+  console.log(input);  
+  return {...input, retrievedAnyItem : (input.tickers.find(item => item.itemsLoaded > 0) != undefined ? "Y" : "N")};
+}
+
+interface Input {
+  tickers: {ticker: string; itemsLoaded: number}[];
+}
+
+interface Output extends Input {
+  retrievedAnyItem: string;
 }
