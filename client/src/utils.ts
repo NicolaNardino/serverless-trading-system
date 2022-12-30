5b537d6b6c8679011cf5b79069dcb8f2caf77231
Nicola Nardino <nicola.nardino@nicolanardino.net>
2022-12-30 18:52:02 +0100

    Removed pkg import added by mistake.

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,7 +1,6 @@
 import { PutCommand } from 'serverless-trading-system-utility-layer/util/dependencies';
 import { getRandomInteger, getRandom, getRandomArrayEntry, getRandomEnum, delay, ddbDocClient } from 'serverless-trading-system-utility-layer/util/utils';
 import { Direction, Type, EntryOrder } from 'serverless-trading-system-utility-layer/util/types';
-import * as fs from 'fs';
 
 const customers = ['000001', '000002', '000003', '000004', '000005', '000006', '000007', '000008', '000009', '0000010', '0000011', '0000012'];
 const tickers = ['ATO', 'BAX', 'BALL', 'WRB', 'BRK.A', 'BRK.B', 'COF', 'CCL', 'CARR', 'CSX', 'DIS', 'EBAY', 'ECL', 'MMM', 'ABT', 'ACN', 'AKAM', 'GOOGL', 'AXP', 'AEP', 'AON', 'AMAT', 'ATO', 'VTV', 'PM', 'BTI', 'META', 'AMD', 'AMZN', 'AAPL', 'GOOG', 'NFLX', 'F', 'T', 'MO', 'PFE', 'COIN', 'MRNA', 'ORCL', 'TSLA', 'SOPH', 'NVDA', 'MSFT', 'UBSG.SW', 'CSGN.SW', 'HOLN.SW', 'CHSPI.SW', 'EOG', 'PBR', 'PBR-A', 'RBLX', 'BABA', 'SPY', 'VTI', 'VOO', 'ADBE'];
