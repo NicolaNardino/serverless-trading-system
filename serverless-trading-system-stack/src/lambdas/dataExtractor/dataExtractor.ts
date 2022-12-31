e8a76ad267ee6c2963f2f0bd8617fe49167749d3
Nicola Nardino <nicola.nardino@nicolanardino.net>
2022-12-31 19:47:42 +0100

    Implemented GetQuoteSummary lambda.

--- serverless-trading-system-stack/src/lambdas/dataExtractor/dataExtractor.ts
+++ serverless-trading-system-stack/src/lambdas/dataExtractor/dataExtractor.ts
@@ -1,24 +1,24 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
 
-import { GetCommand, QueryCommand } from '../../layers/common/util/dependencies.js';
+import { GetCommand, QueryCommand } from '/opt/nodejs/util/dependencies.js';
 import { ddbDocClient } from '/opt/nodejs/util/utils.js';
 
 const tableName = process.env.tradesStoreTableName;
 
 export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-  console.log(event);
+    console.log(event);
     let result;
     const path = event.path;
     if (event.httpMethod === 'GET') {
         const split = path.split('/');
         const customerId = split[2];
-        if (split[1] === 'trades') { 
+        if (split[1] === 'trades') {
             const { tradeDate } = event.queryStringParameters;
             const params = {
                 TableName: tableName,
                 ExpressionAttributeValues: {
-                    ":customerId": "CUST#"+customerId,
-                    ":tradeDate": "TRADE#"+tradeDate
+                    ":customerId": "CUST#" + customerId,
+                    ":tradeDate": "TRADE#" + tradeDate
                 },
                 KeyConditionExpression: "PK = :customerId and begins_with(SK, :tradeDate)"
             };
@@ -28,8 +28,8 @@ export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayPr
             const params = {
                 TableName: tableName,
                 Key: {
-                    PK: "CUST#"+customerId, 
-                    SK: "CUST#"+customerId
+                    PK: "CUST#" + customerId,
+                    SK: "CUST#" + customerId
                 }
             };
             result = (await ddbDocClient.send(new GetCommand(params))).Item;
--- serverless-trading-system-stack/src/lambdas/marketData/marketDataAPI/getQuoteSummary/getQuoteSummary.ts
+++ serverless-trading-system-stack/src/lambdas/marketData/marketDataAPI/getQuoteSummary/getQuoteSummary.ts
@@ -1,13 +1,24 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
 
+import { GetCommand } from '/opt/nodejs/util/dependencies.js';
+import { ddbDocClient } from '/opt/nodejs/util/utils.js';
+
+const marketDataTableName = process.env.marketDataTableName;
+
 /**
  * .../quote-summary/{ticker}
 */
 export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-    const result = {};
     const { ticker } = event.pathParameters;
+    const params = {
+        TableName: marketDataTableName,
+        Key: {
+            PK: "TICKER#" + ticker,
+            SK: "SUMMARY#" + ticker
+        }
+    };
     return {
         statusCode: 200,
-        body: JSON.stringify(result)
+        body: JSON.stringify((await ddbDocClient.send(new GetCommand(params))).Item)
     };
 }
